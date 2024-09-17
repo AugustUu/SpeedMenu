4bceaaca75195098561a74f1d4683d563c655d33
AugustUu <98927957+AugustUu@users.noreply.github.com>
2024-09-16 22:49:25 -0700

    added popups to gameplay

---
+++ Assets/Scripts/ClockScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class ClockScript : MonoBehaviour
+{
+    TMP_Text text;
+    int min = 0;
+    int seconds = 10;
+
+    public GameObject popup;
+    public GameObject desktop;
+
+
+    void Start()
+    {
+        text = this.GetComponentInChildren<TMP_Text>();
+        InvokeRepeating("tickClock",0f,1f);
+    }
+
+    void win(){
+       Desktop desktop_script = desktop.GetComponent<Desktop>();
+
+        float avarge = 0;
+
+       foreach(var icon in desktop_script.icons){
+            avarge += icon.health;
+       }
+       avarge /= desktop_script.icons.Count;
+
+       Debug.Log("score " + avarge);
+    }
+
+    void tickClock(){
+        if(min == 0 && seconds == 0){
+            win();
+            CancelInvoke("tickClock");
+        }
+
+        if(seconds == 0){
+            min -= 1;
+            seconds = 59;
+        }else{
+            seconds -= 1;
+        }
+        text.text = "" + min + ":" + seconds;
+
+        if(Random.Range(0,3 + min) == 0){
+            Vector3 test = new Vector3(Random.Range(200, Screen.width -200), Random.Range(200, Screen.height -200), 0);
+            GameObject new_popup = Instantiate(popup, desktop.transform);
+            new_popup.transform.position = test;
+        }
+    }
+    
+}
